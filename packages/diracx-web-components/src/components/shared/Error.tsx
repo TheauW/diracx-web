968c41e04e1f18d61938c946ea613a414b7fe556
theau <theau.laurent.wartel@cern.ch>
2025-04-28 15:45:03 +0200

    feat: create the  error component

---
+++ packages/diracx-web-components/src/components/shared/Error.tsx
@@ -0,0 +1,43 @@
+import { Box, Alert, AlertTitle, Button } from "@mui/material";
+import ReplayIcon from "@mui/icons-material/Replay";
+
+/**
+ * This component is used to display an error message
+ *
+ * @param s The error message (optional)
+ * @param reset The function to call to reset the error (optional)
+ * @returns Error component
+ */
+export function ErrorComponent({
+  msg,
+  reset,
+}: {
+  msg?: string;
+  reset?: () => void;
+}) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+      onClick={reset && reset}
+    >
+      <Alert
+        severity="error"
+        action={
+          reset && (
+            <Button sx={{ color: "black" }} onClick={reset}>
+              <ReplayIcon />
+            </Button>
+          )
+        }
+      >
+        <AlertTitle>Error</AlertTitle>
+        {msg ? msg : "Something went wrong"}
+      </Alert>
+    </Box>
+  );
+}
--- packages/diracx-web-components/src/components/shared/index.ts
+++ packages/diracx-web-components/src/components/shared/index.ts
@@ -1,3 +1,4 @@
 export { DataTable } from "./DataTable";
 export { FilterForm } from "./FilterForm";
 export { FilterToolbar } from "./FilterToolbar";
+export { ErrorComponent } from "./Error";
